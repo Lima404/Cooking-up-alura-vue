6ea7e3ba30a7be12734344cc6e3f05ae502a3bb3
Lima404 <gabriel.lima2307@uol.com.br>
2024-04-05 00:29:37 -0300

    add state onclick in ingredientes buttons

---
+++ src/components/IngredienteSelecionavel.vue
@@ -0,0 +1,33 @@
+<template>
+    <button
+        class="ingrediente"
+        @click="selecionado = !selecionado"
+        :area-pressed="selecionado"
+    >
+        <Tag :texto="ingrediente" :ativa="selecionado" />
+    </button>
+</template>
+
+<script lang="ts">
+import Tag from './Tag.vue';
+
+    export default {
+        components: { Tag },
+        props: {
+            ingrediente: { type: String, required: true }
+        },
+        data(){
+            return{
+                selecionado: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+.ingrediente {
+    cursor: pointer;
+}
+
+</style>
