5eb29970b01db4abe62c2530ddf8c6e7d3a35dd4
Lima404 <gabriel.lima2307@uol.com.br>
2024-08-25 19:51:55 -0300

    álgum commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <script lang="ts">
-
-  import Banner from '@/components/Banner.vue';
+  import Banner from './components/Banner.vue';
   import ConteudoPrincipal from './components/ConteudoPrincipal.vue'
-  import Rodape from './components/Rodape.vue';
+  import Rodape from './components/Rodape.vue'
 
 export default {
   components: { Banner, ConteudoPrincipal, Rodape }
